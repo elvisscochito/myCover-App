cb2f82bd4f9eed893a1a78cd210af9971ec0d9f0
elvisscochito <elvirodominguezsoriano@outlook>
2024-10-22 18:57:31 -0600

    Initial Commit

---
+++ mycover-app/mycover_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  mycover_appApp.swift
+//  mycover-app
+//
+//  Created by Alumno on 22/10/24.
+//
+
+import SwiftUI
+
+@main
+struct mycover_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ mycover-appTests/mycover_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  mycover_appTests.swift
+//  mycover-appTests
+//
+//  Created by Alumno on 22/10/24.
+//
+
+import Testing
+@testable import mycover_app
+
+struct mycover_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
