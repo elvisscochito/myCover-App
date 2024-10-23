45ad4e5488c24c3417775877fcd5d8f3cb674201
elvisscochito <elvirodominguezsoriano@outlook>
2024-10-22 19:36:41 -0600

    Wireframe | Joel

--- mycover-app/ContentView.swift
+++ mycover-app/ContentView.swift
@@ -1,21 +1,28 @@
-//
-//  ContentView.swift
-//  mycover-app
-//
-//  Created by Alumno on 22/10/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var navigateToHome = false
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            // Navegación
+            if navigateToHome {
+                Home() // Vista a la que se navega después de 3 segundos
+            } else {
+                ZStack {
+                    
+                    Text("Icono Logo")
+
+                
+                }
+                .onAppear {
+                    // Navegar a Home después de 3 segundos
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                        navigateToHome = true
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
---
+++ mycover-app/Home.swift
@@ -0,0 +1,24 @@
+//
+//  Home.swift
+//  mycover-app
+//
+//  Created by Alumno on 22/10/24.
+//
+
+import SwiftUI
+
+struct Home: View {
+    
+    var body: some View {
+        NavigationView {
+            // Sección de saldo y botón interactivo con Maya
+            VStack {
+                HStack {
+                    // Mostrar el saldo actual dinámicamente desde SaldoManager
+                    Text("Home")
+                }
+                
+                
+                
+                
+            }}}}
