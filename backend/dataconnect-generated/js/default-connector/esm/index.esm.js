d1a5b91d96692a0e229edd5f1a80352f7b3efe3f
Joel Vargas Reynoso <76189923+joelvshimself@users.noreply.github.com>
2024-11-15 21:40:03 -0600

    elvis core
    Co-Authored-By: Elviro Dominguez Soriano <elvirodominguezsoriano@outlook.com>

---
+++ backend/dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { getDataConnect, validateArgs } from 'firebase/data-connect';
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'backend',
+  location: 'us-central1'
+};
+
